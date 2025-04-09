828fbf1aa08bdc6ad3bb95d68a5e2c48fc817937
InstaAstro <manish@instaastro.in>
2025-04-09 10:30:40 +0530

    hot fixes

---
+++ src/components/AndroidCubeEffect.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AndroidCubeEffect = () => {
+  return (
+    <View>
+      <Text>AndroidCubeEffect</Text>
+    </View>
+  )
+}
+
+export default AndroidCubeEffect
+
+const styles = StyleSheet.create({})
---
+++ src/components/CubeNavigationHorizontal.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CubeNavigationHorizontal = () => {
+  return (
+    <View>
+      <Text>CubeNavigationHorizontal</Text>
+    </View>
+  )
+}
+
+export default CubeNavigationHorizontal
+
+const styles = StyleSheet.create({})
